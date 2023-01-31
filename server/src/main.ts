3191978d5a7828116aadc5f2aeca7ae9fe89efdb
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-01-31 21:54:46 +0100

    add cors configuration and get origins from .env variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const options = {
+    origin:process.env.CLIENT_ORIGIN, //[arr of origins]
+    credentails:true
+  }
+
+
+  //enabling cors
+  app.enableCors(options)
+  
   await app.listen(process.env.PORT || 3000);
 }
 try {
