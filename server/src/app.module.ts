62459661bf0dc2312af84a485b6e690dfe50aeb9
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-01-31 22:03:07 +0100

    generate controller and service of the explorer

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service'
 import {ConfigModule} from '@nestjs/config'
+import { ExplorerModule } from './explorer/explorer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true
-    })
+    }),
+    ExplorerModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/explorer/explorer.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('explorer')
+export class ExplorerController {}
