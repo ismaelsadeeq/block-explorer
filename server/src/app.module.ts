bf3e7f8e88ffaf46d1bbeb9c05ed4fba074e7979
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-04 23:36:10 +0100

    import the bitcoindInterfaceModule in app Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service'
 import {ConfigModule} from '@nestjs/config'
 import { ExplorerModule } from './explorer/explorer.module';
+import { BitcoindInterfaceModule } from './bitcoind-interface/bitcoind-interface.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true
     }),
-    ExplorerModule
+    ExplorerModule,
+    BitcoindInterfaceModule
   ],
   controllers: [AppController],
   providers: [AppService],
