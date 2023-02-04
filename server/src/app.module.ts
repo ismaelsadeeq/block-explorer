60615fb7445a0324a59c305505f46e0e67bdacc1
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-04 23:37:14 +0100

    remove bitcoind-interface module in app module.ts
    bitcoind-interface module is only used in explorer module
    so no need to import it in the app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,15 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service'
 import {ConfigModule} from '@nestjs/config'
 import { ExplorerModule } from './explorer/explorer.module';
-import { BitcoindInterfaceModule } from './bitcoind-interface/bitcoind-interface.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true
     }),
-    ExplorerModule,
-    BitcoindInterfaceModule
+    ExplorerModule
   ],
   controllers: [AppController],
   providers: [AppService],
