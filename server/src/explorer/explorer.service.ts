306ead6bf026db8f7aae5c62dc39aa845bf317be
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-05 23:02:52 +0100

    implement search for a block height controller and service

--- server/src/explorer/explorer.service.ts
+++ server/src/explorer/explorer.service.ts
@@ -3,6 +3,7 @@ import { BitcoindInterfaceService } from 'src/bitcoind-interface/bitcoind-interf
 import { ResponseData } from 'src/response-handler/interface/response.handler.interface';
 import { ResponseHandlerService } from 'src/response-handler/response-handler.service';
 import { Meta } from 'src/response-handler/interface/response.handler.interface';
+import { responseType } from 'src/bitcoind-interface/interface';
 
 @Injectable()
 export class ExplorerService {
@@ -15,15 +16,15 @@ export class ExplorerService {
   async getTopBlocks():Promise<ResponseData>{
     try {
       
-      const chainTipMethod:string = "getbestblockhash"
+      const chainTipHash:string = "getbestblockhash"
       let parameters:Array<string> = []
-      const chainTip = await this.bitcoindService.bitcoindGet(chainTipMethod,parameters)
+      const chainTip:responseType<any>  = await this.bitcoindService.bitcoindGet(chainTipHash,parameters)
       let currentHash:string = chainTip.data.result
       
       const blocks:Array<any> = []
       const method:string = "getblock"
       parameters = [currentHash]
-      let block:any = await this.bitcoindService.bitcoindGet(method,parameters)
+      let block:responseType<any>  = await this.bitcoindService.bitcoindGet(method,parameters)
       blocks.push(block.data.result)
 
       for(let i = 0;i<4;i++){
@@ -54,22 +55,24 @@ export class ExplorerService {
 
   async getTopTransaction():Promise<ResponseData>{
     try  {
-      const chainTipMethod:string = "getchaintips"
-      let parameters:Array<string> = []
-      const chainTip = await this.bitcoindService.bitcoindGet(chainTipMethod,parameters)
+      const chainTipHash:string = "getbestblockhash"
+      let parameters:Array<string| boolean> = []
+      const chainTip:responseType<any> = await this.bitcoindService.bitcoindGet(chainTipHash,parameters)
 
-      let currentHash:string = chainTip.data.result[0].hash
+      let currentHash:string = chainTip.data.result
       let method:string = "getblock"
       parameters = [currentHash]
       let transactions: Array<string | any> = []
-      let block = await this.bitcoindService.bitcoindGet(method,parameters)
+      let block:responseType<any>  = await this.bitcoindService.bitcoindGet(method,parameters)
       transactions = [...block.data.result.tx]
+
       while(transactions.length < 5){
         parameters = [block.data.result.previousblockhash]
         block = await this.bitcoindService.bitcoindGet(method,parameters)
         transactions.push(...block.data.result.tx)
       }
       method = 'getrawtransaction';
+
       for(let i=0;i<transactions.length;i++){
         parameters = [transactions[i],true]
         const transaction = await this.bitcoindService.bitcoindGet(method,parameters)
@@ -89,6 +92,33 @@ export class ExplorerService {
       const response:Meta = {
         status:false,
         message:"failed to get transactions",
+        pagination:undefined
+      }
+      return this.responseHandlerService.responseBody(undefined,response)
+    }
+  }
+  async searchBlockHeight(height:number):Promise<ResponseData>{
+    try {
+      let method:string = "getblockhash"
+      let parameters:Array<number | boolean | string> = [height]
+      const blockHash:string = (await this.bitcoindService.bitcoindGet(method,parameters)).data.result
+      method = 'getblock'
+      parameters = [blockHash,true];
+      const block:any =  (await this.bitcoindService.bitcoindGet(method,parameters)).data.result
+
+      const response:Meta = {
+        status:true,
+        message:"success",
+        pagination:undefined
+      }
+
+      return this.responseHandlerService.responseBody(block,response);
+
+    } catch (error:unknown) {
+      Logger.log(error)
+      const response:Meta = {
+        status:false,
+        message:"failed to get block",
         pagination:undefined
       }
       return this.responseHandlerService.responseBody(undefined,response)
