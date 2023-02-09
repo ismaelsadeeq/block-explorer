edb98ced175a24cafb3760865c0265c621a66cc5
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-09 23:09:19 +0100

    create explorer interface file and add decodeBase58check interface

--- server/src/explorer/explorer.service.ts
+++ server/src/explorer/explorer.service.ts
@@ -4,10 +4,17 @@ import { ResponseData } from 'src/response-handler/interface/response.handler.in
 import { ResponseHandlerService } from 'src/response-handler/response-handler.service';
 import { Meta } from 'src/response-handler/interface/response.handler.interface';
 import { responseType } from 'src/bitcoind-interface/interface';
+import { decodedBase58Check } from './interface';
+import * as crypto from 'crypto'
 
 @Injectable()
 export class ExplorerService {
-
+  private readonly base58check:any = require('base58check')
+  private readonly OP_DUP:string = "76"; 
+  private readonly OP_HASH160:string = "a9";
+  private readonly OP_EQUALVERIFY:string = "88";
+  private readonly OP_CHECKSIG:string ="ac"
+  private readonly TO_PUSH:string = '14'
   constructor(
     private readonly responseHandlerService:ResponseHandlerService = ResponseHandlerService.instance,
     private readonly bitcoindService:BitcoindInterfaceService
@@ -173,10 +180,13 @@ export class ExplorerService {
       return this.responseHandlerService.responseBody(undefined,response)
     }
   }
-  async getElectrsVersion():Promise<ResponseData>{
+  async getAddressBalance(address:string):Promise<ResponseData>{
     try  {
-      const parameters:Array<string> = ["", "1.4"]
-      const method:string = "server.version"
+      const scriptHash:string = this.scriptHash(address);
+
+      const parameters:Array<string> = [scriptHash];
+
+      const method:string = "blockchain.scripthash.get_balance";
       const block:string = await this.bitcoindService.electr(parameters,method)
   
       const response:Meta = {
@@ -196,5 +206,60 @@ export class ExplorerService {
       }
       return this.responseHandlerService.responseBody(undefined,response)
     }
+  }
+  scriptHash(address:string):string{
+    const prefix:string = address.slice(0,1)
+    switch(prefix){
+      case  "1":{
+        return this.getP2PKH(address)
+      }
+      // To be implemented
+      case "3":{
+        return "script"
+      }
+      // To be implemeted
+      case "b":{
+        return "witness"
+      }
+    }
+  }
+  getP2PKH(address:string):string{
+    //Decode the address to a public key hash
+    let { data }:decodedBase58Check = this.base58check.decode(address,"hex");
+
+    //Create a script pub by adding the opcodes for a P2PKH
+    let scriptPubKey = this.OP_DUP + this.OP_HASH160 + this.TO_PUSH + data + this.OP_EQUALVERIFY + this.OP_CHECKSIG;
+
+    //Hash the script pub key with sha256
+    scriptPubKey = crypto.createHash('sha256')
+    .update(Buffer.from(scriptPubKey, 'hex'))
+    .digest('hex');
+
+    //Reverse the hash of the scriptPubKey
+    scriptPubKey = this.reverseHash(scriptPubKey)
+
+    //Return the reversed hash as the script pub key.
+    return scriptPubKey
+  }
+  reverseHash(scriptHash:string):string{
+    //create reversed hash string
+    let reversedHash:string = '';
+    /*Loop througt the hash if and reverse it.
+
+    The reversal algorithm takes two characters from the end, reverse them and append.
+    to the reversedHash variable untill the characters are exhausted.
+
+    e.g df018b is 8b01df
+    
+    The scriptHash is 64 characters hence if it's odd the its at the
+    second character of the first two characters to be reversed you append the 
+    first character, but if it's even its at the first, which is already appended,
+    hence the second character is appended.
+    */ 
+    for(let i:number = scriptHash.length - 1;i>=0;i--){
+      
+      reversedHash += i % 2 != 0 ? scriptHash[i-1]:scriptHash[i+1]
+    }
+    return reversedHash;
   }
 }
