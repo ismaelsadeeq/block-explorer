a87f29e347c13b435e12e517b90d0feb656ba80f
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-10 18:15:33 +0100

    decode P2WPKH, P2WSH  and convert it to a script hash
    this decoding can support P2WPKH P2WSH with version 0 and 1 segwit.
    
    version 1 segwit that is P2TR.

--- server/src/explorer/explorer.service.ts
+++ server/src/explorer/explorer.service.ts
@@ -5,17 +5,20 @@ import { ResponseHandlerService } from 'src/response-handler/response-handler.se
 import { Meta } from 'src/response-handler/interface/response.handler.interface';
 import { responseType } from 'src/bitcoind-interface/interface';
 import { decodedBase58Check } from './interface';
-import * as crypto from 'crypto'
-
+import * as crypto from 'crypto';
+import { bech32, Decoded } from 'bech32';
 @Injectable()
 export class ExplorerService {
   private readonly base58check:any = require('base58check')
   private readonly OP_DUP:string = "76"; 
   private readonly OP_HASH160:string = "a9";
   private readonly OP_EQUALVERIFY:string = "88";
-  private readonly OP_CHECKSIG:string ="ac"
-  private readonly TO_PUSH:string = '14'
-  private readonly OP_EQUAL:string = "87"
+  private readonly OP_CHECKSIG:string ="ac";
+  private readonly TO_PUSH:string = '14';
+  private readonly OP_EQUAL:string = "87";
+  private readonly SEGWIT_VERSION_0 = "00";
+  private readonly SEGWIT_VERSION_1 = "51";
+
   constructor(
     private readonly responseHandlerService:ResponseHandlerService = ResponseHandlerService.instance,
     private readonly bitcoindService:BitcoindInterfaceService
@@ -46,6 +49,7 @@ export class ExplorerService {
         message:"success",
         pagination:undefined
       }
+
 
       return this.responseHandlerService.responseBody(blocks,response);
 
@@ -184,7 +188,7 @@ export class ExplorerService {
   async getAddressBalance(address:string):Promise<ResponseData>{
     try  {
       const scriptHash:string = this.scriptHash(address);
-
+      Logger.log(scriptHash)
       const parameters:Array<string> = [scriptHash];
 
       const method:string = "blockchain.scripthash.get_balance";
@@ -211,23 +215,21 @@ export class ExplorerService {
     const prefix:string = address.slice(0,1)
     switch(prefix){
       case  "1":{
-        return this.getP2PKH(address)
+        return this.getP2PKH(address);
       }
-      // To be implemented
       case "3":{
-        return this.getP2SH(address)
+        return this.getP2SH(address);
       }
-      // To be implemeted
       case "b":{
-        return "witness"
+        return this.getSegwitAddress(address);
       }
     }
   }
   getP2PKH(address:string):string{
-    //Decode the address to a public key hash
+    //Decode the address to get the public key hash
     let { data }:decodedBase58Check = this.base58check.decode(address,"hex");
 
-    //Create a script pub by adding the opcodes for a P2PKH
+    //Create a script pub key  by adding the opcodes for a P2PKH
     let scriptPubKey = this.OP_DUP + this.OP_HASH160 + this.TO_PUSH + data + this.OP_EQUALVERIFY + this.OP_CHECKSIG;
 
     //Hash the script pub key with sha256
@@ -239,19 +241,51 @@ export class ExplorerService {
     return this.reverseHash(scriptPubKey);
   }
   getP2SH(address:string):string {
+    //Decode the address to get the script hash
 
     let {data}:decodedBase58Check = this.base58check.decode(address,'hex')
 
-    let scriptHash:string = this.OP_HASH160 + this.TO_PUSH + data + this.OP_EQUAL;
+     //Create a script pub key by adding the opcodes for a P2SH
+    let scriptPubKey:string = this.OP_HASH160 + this.TO_PUSH + data + this.OP_EQUAL;
 
-    scriptHash = crypto.createHash('sha256')
-    .update(Buffer.from(scriptHash,'hex'))
+    //Hash the script pub key with sha256
+    scriptPubKey = crypto.createHash('sha256')
+    .update(Buffer.from(scriptPubKey,'hex'))
     .digest('hex')
 
-    return this.reverseHash(scriptHash)
+     //Reverse and return  the hash of the scriptPubKey
+    return this.reverseHash(scriptPubKey)
 
   }
-  reverseHash(scriptHash:string):string{
+  getSegwitAddress(address:string):string {
+
+    //Decode the address to get the Decode object wich has the words and prefix
+    const decoded:Decoded = bech32.decode(address);
+
+    //Get the version script hex from the get segwitversion method
+    const versionScript:string =  this.getSegwitVersionScript(decoded.words[0])
+
+    // Slice the data and get the witness program array.
+    const witnessProgram:Array<number> = decoded.words.slice(1);
+
+    // Encode the witness program
+    const encoded:Array<number> = bech32.fromWords(witnessProgram);
+
+    // convert the witness program to hex
+    const witnessProgramHex:string = Buffer.from(encoded).toString('hex');  
+
+    //Create a script pub key by adding the opcodes for a P2WPKH
+    let scriptPubKey:string =  versionScript + this.TO_PUSH + witnessProgramHex;
+     //Hash the script pub key with sha256
+     scriptPubKey = crypto.createHash('sha256')
+     .update(Buffer.from(scriptPubKey,'hex'))
+     .digest('hex')
+
+    //Reverse and return the hash of the scriptPubKey
+    return this.reverseHash(scriptPubKey)
+
+  }
+  reverseHash(scriptPubKey:string):string{
     //create reversed hash string
     let reversedHash:string = '';
     /*Loop througt the hash if and reverse it.
@@ -261,16 +295,25 @@ export class ExplorerService {
 
     e.g df018b is 8b01df
     
-    The scriptHash is 64 characters hence if it's odd the its at the
+    The scriptPubKey is 64 characters hence if it's odd the its at the
     second character of the first two characters to be reversed you append the 
     first character, but if it's even its at the first, which is already appended,
     hence the second character is appended.
     */ 
-    for(let i:number = scriptHash.length - 1;i>=0;i--){
+    for(let i:number = scriptPubKey.length - 1;i>=0;i--){
       
-      reversedHash += i % 2 != 0 ? scriptHash[i-1]:scriptHash[i+1]
+      reversedHash += i % 2 != 0 ? scriptPubKey[i-1]:scriptPubKey[i+1]
     }
     return reversedHash;
+  }
+  getSegwitVersionScript(version:number):string {
+    switch(version){
+      case 0:
+        return this.SEGWIT_VERSION_0
+      
+      case 1:
+        return this.SEGWIT_VERSION_1
+    }
   }
 }
 
