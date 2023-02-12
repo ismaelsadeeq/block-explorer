74ff801040cb38bfd5d826550d724d4ace9ea971
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-12 15:41:03 +0100

    add comment to interface file

--- server/src/explorer/interface.ts
+++ server/src/explorer/interface.ts
@@ -1,3 +1,6 @@
+//The explorer helper interfaces file.
+
+//The interfaces for a decoded base58check which has a data and prefix. 
 export interface decodedBase58Check {
   data:string,
   prefix:string
