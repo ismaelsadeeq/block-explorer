56a808cd9dd0e655c4480541a2285377416b6497
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-12 15:54:08 +0100

    added comments to bitcoind-interface service

--- server/src/bitcoind-interface/bitcoind-interface.service.ts
+++ server/src/bitcoind-interface/bitcoind-interface.service.ts
@@ -7,37 +7,47 @@ import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class BitcoindInterfaceService {
-  private ElectrumCli:any = require('electrum-client')
+
+  private readonly ElectrumCli:any = require('electrum-client')
 
   constructor(
     private readonly httpService: HttpService,
   ){}
 
   async bitcoindGet(method:string,params:Array<any>):Promise<responseType<any,any>>{
-    let payload:configData = new getConfig(params,method).getConfig(); 
-    let response:responseType<any,any> = await firstValueFrom(
+
+    // create a payload of bitcoind parameters with the getConfig class
+    const payload:configData = new getConfig(params,method).getConfig(); 
+    // call the bitcoin-d rpc via  http service post request.
+    const response:responseType<any,any> = await firstValueFrom(
+
       this.httpService.post(process.env.URL,JSON.stringify(payload)).pipe(
         catchError((error: AxiosError) => {
           Logger.error(error.response.data);
             throw 'An error happened!';
           }),
+
         ),
     );
     return response;
   }
   async electr(params :Array<string>,method:string): Promise<any> {
 
-    const ecl:any = new this.ElectrumCli(parseInt(process.env.ELECTR_PORT), process.env.ELECTR_HOST, process.env.PROTOCOL) // tcp or tls
-    await ecl.connect() // connect(promise)
-    ecl.subscribe.on(method,(v) => console.log(v)) // subscribe message(EventEmitter)
+    // Create a tcp connection with electrs
+    const electrs:any = new this.ElectrumCli(parseInt(process.env.ELECTR_PORT), process.env.ELECTR_HOST, process.env.PROTOCOL) // tcp or tls
+    await electrs.connect() // connect(promise)
+
+    // Call the electrs rpc with the required method and params
+    electrs.subscribe.on(method,(v) => console.log(v)) // subscribe message(EventEmitter)
     try{
-        const response:any = await ecl.electr_client(method,params) // json-rpc(promise)
-        await ecl.close()
+        const response:any = await electrs.electr_client(method,params) // json-rpc(promise)
+        //Close the tcp connection
+        await electrs.close()
         return response
 
     }catch(e){
         Logger.log(e)
-        await ecl.close()
+        await electrs.close()
         return {
           "status":false,
           "message":e
