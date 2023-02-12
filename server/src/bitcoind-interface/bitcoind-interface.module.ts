1903ac462995d4c0e7f1c1be5b180fae34520fc8
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-12 15:44:43 +0100

    add comments to explain bitcoin-interface module register async

--- server/src/bitcoind-interface/bitcoind-interface.module.ts
+++ server/src/bitcoind-interface/bitcoind-interface.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@nestjs/axios';
 @Module({
   imports:[
     HttpModule,
+    //Enable http Module to be asynchronous and timeout after 5s and set 5 max redirects
     HttpModule.registerAsync({
       useFactory:()=>({
         timeout: 5000,
