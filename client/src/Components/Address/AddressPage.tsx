c7008e170ece3201f2c04aebdffcafc262fa865f
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-15 12:56:24 +0100

    implement address page and connect to API

--- client/src/Components/Address/AddressPage.tsx
+++ client/src/Components/Address/AddressPage.tsx
@@ -5,9 +5,27 @@ import { Button, CssBaseline, Box, Typography, IconButton } from "@mui/material"
 import { Container } from "@mui/system"
 import {  ArrowBackIosNewRounded } from "@mui/icons-material"
 import AddressView from "./AddressView"
+import { NavigateFunction, useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { getRequest } from "../../Utilities/Network"
+import { url } from "../../Utilities/util"
 
 function AddressPage() {
-
+  const navigate:NavigateFunction = useNavigate();
+  const {address} = useParams()
+  const [balance,setBalance] = useState(null)
+  const [transactions,setTransactions] =  useState(null);
+  async function getData(){
+    let method:string = url + "/address-balance/" + address
+    const balance = await getRequest(method)
+    method = url +"/address-transactions/" + address
+    const transactions =  await getRequest(method)
+    setBalance(balance)
+    setTransactions(transactions)
+  }
+  useEffect(()=>{
+    getData()
+  },[address]);
   return (
     <Box
     sx={{
@@ -28,7 +46,9 @@ function AddressPage() {
         display:'flex',
         justifyContent:'space-between'
       }}>
-        <Button >
+        <Button  onClick={()=>{
+            navigate(-1)
+          }}>
          <IconButton >
             <ArrowBackIosNewRounded />
          </IconButton>
@@ -37,7 +57,7 @@ function AddressPage() {
         Address
       </Typography>
       </Container>
-      <AddressView />
+      <AddressView address={address} balance={balance} transactions={transactions} />
     </Container>
     <Footer />
   </Box>
